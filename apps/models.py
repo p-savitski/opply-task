b9bc27e48157e0223b6ec6700eeaff228720f04b
paveloush <pavel.savitski@gmail.com>
2022-04-28 23:02:51 +0200

    fix models imports

--- apps/models.py
+++ apps/models.py
@@ -1,2 +1,5 @@
 #       Required for Django monoapp strategy.
 #       To import model class, use direct imports from the apps' models.
+from .users import models as _users_models
+from .orders import models as _orders_models
+from .products import models as _products_models
